fc42b8c04a69cfb9013f7b29a4a85a62bdd58211
Sarah Wood <wood.sarah@outlook.com>
2020-02-26 18:54:35 +0000

    models added

---
+++ headsets/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+# Create your models here.
+class Headset(models.Model):
+    name = models.CharField(max_length=50)
+    image = models.CharField(max_length=300)
+    headset_type = models.CharField(max_length=50)
+    connections = models.CharField(max_length=50)
+    resolution = models.CharField(max_length=50)
+    refresh_rate = models.CharField(max_length=50)
+    motion_tracking = models.CharField(max_length=50)
+    controls = models.CharField(max_length=50)
+    hardware_platform = models.CharField(max_length=50)
+    software_platform = models.CharField(max_length=50)
+    description = models.CharField(max_length=500)
