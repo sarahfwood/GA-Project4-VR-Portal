9b722f107dbc248391a3aa9fc0ee6c5e14ed913d
Sarah Wood <wood.sarah@outlook.com>
2020-02-26 22:46:04 +0000

    show route added - detail view

--- headsets/urls.py
+++ headsets/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import ListView
+from .views import ListView, DetailView
 
 urlpatterns = [
     path('', ListView.as_view()),
+    path('<int:pk>/', DetailView.as_view()),
 ]
--- headsets/views.py
+++ headsets/views.py
@@ -10,3 +10,11 @@ class ListView(View):
         headsets = Headset.objects.all()
 
         return render(request, 'index.html', {'headsets': headsets})
+
+
+class DetailView(View):
+  
+    def get(self, request, pk):
+        headset = Headset.objects.get(pk=pk)
+
+        return render(request, 'show.html', {'headset': headset})
