250502b99462f72b966ecab2c1d8b5cacc71ae0d
Sarah Wood <wood.sarah@outlook.com>
2020-02-26 21:37:32 +0000

    index.html updated

--- headsets/views.py
+++ headsets/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
 from django.views import View
 
+from .models import Headset
+
 # Create your views here.
 class ListView(View):
 
     def get(self, request):
-        return render(request, 'index.html')
+        headsets = Headset.object.all()
+
+        return render(request, 'index.html', {'headsets': headsets})
