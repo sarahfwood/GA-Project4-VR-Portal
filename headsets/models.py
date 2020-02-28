c0bb334af3793ffa60beeb7b873d28b019fa77db
Sarah Wood <wood.sarah@outlook.com>
2020-02-28 13:19:26 +0000

    models updated

--- headsets/models.py
+++ headsets/models.py
@@ -1,10 +1,16 @@
 from django.db import models
 
 # Create your models here.
+class Type(models.Model):
+    headset_type = models.CharField(max_length=50)
+
+    def __str__(self):
+        return f'{self.headset_type}'
+
 class Headset(models.Model):
     name = models.CharField(max_length=50)
     image = models.CharField(max_length=300)
-    headset_type = models.CharField(max_length=50)
+    headset_type = models.ForeignKey(Type, related_name='headsets', on_delete=models.CASCADE)
     connections = models.CharField(max_length=50)
     resolution = models.CharField(max_length=50)
     refresh_rate = models.CharField(max_length=50)
