979caef5d5451b61058ce6ec4cf25d23eb02fe97
Sarah Wood <wood.sarah@outlook.com>
2020-03-02 22:54:18 +0000

    serializers updated

--- headsets/serializers.py
+++ headsets/serializers.py
@@ -1,8 +1,16 @@
 from rest_framework import serializers
 from .models import Headset
 
+class TypeSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Type
+        fields = ('model_type')
+
 class HeadsetSerializer(serializers.ModelSerializer):
 
+    headset_type = TypeSerializer()
+    
     class Meta:
         model = Headset
         fields = ('name', 'image', 'headset_type', 'connections', 'resolution', 'refresh_rate', 'motion_tracking', 'controls', 'hardware_platform', 'software_platform', 'description')
